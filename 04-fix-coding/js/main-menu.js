72556519ceb900f8cf1c59db08fda2a13053694c
Aida N <nacarinobazaga@gmail.com>
2021-03-11 09:27:23 +0100

    Modifico creando un método para renderinzar individualmente los items del menú

--- 04-fix-coding/js/main-menu.js
+++ 04-fix-coding/js/main-menu.js
@@ -11,15 +11,20 @@ class MainMenu extends HTMLElement {
     }
 
     _render() {
-        // display elements
-        const linkElement = document.createElement('a');
-        linkElement.setAttribute('href', '#trending');
-        linkElement.innerText = 'Popular';
+        this._addMenuItem('#trending', 'Popular');
+    }
 
+    _addMenuItem(textHref, textMenu) {
+        // display elements
+        // link
+        const linkElement = document.createElement('a');
+        linkElement.setAttribute('href', textHref);
+        linkElement.innerText = textMenu;
+        // item list
         const listElem = document.createElement('li');
         listElem.classList.add('navbar-item');
         listElem.appendChild(linkElement);
-
+        // append into custom element
         this.appendChild(listElem);
     }
 }
